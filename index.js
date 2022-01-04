fe4a5f738ed63c693d92cc29bdc482ac946154ea
Mehedi Hasan Saikat <29018897+mehedi-hasan-saikat@users.noreply.github.com>
2022-01-04 23:42:03 +0600

    Readme File Update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 require("dotenv").config();
 const stripe = require("stripe")(process.env.STRYPE_SECRET);
 
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
